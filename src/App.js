563f25f223cae93806bea4c7ee6ec75a9e18e889
skarabom <makgobask2002@gmail.com>
2025-08-13 18:14:44 +0200

    First configeration

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Main from './Components/Main.js'
+function App() {
+  return (
+      <Main />
+  );
+}
+
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,53 @@
+import Logo from "./img/favicon.png"
+import React, { useState } from "react";
+
+const Main = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <div class="relative bg-white dark:bg-black text-black dark:text-white font-[family-name: Supreme] flex flex-col items-center" >
+            <div className="NavBar" class="grid grid-cols-2 gap-2 md:gap-15 h-20 bg-white dark:bg-black text-black dark:text-white min-w-100 lg:w-4/5 md:my-auto">
+                <ul class="my-auto mx-2 md:mx-5">
+                    <li>
+                        <span class="flex flex-row">
+                            <img src={Logo} alt="icon" class="h-10"/>
+                            <span class="my-auto ml-5"></span>
+                        </span>
+                    </li>
+                </ul>
+                <div className="flex justify-end items-center md:hidden">
+                    <button
+                        className="p-2"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        aria-label="Toggle menu"
+                    >
+                        <span className="block w-6 h-0.5 bg-black dark:bg-white mb-1"></span>
+                        <span className="block w-6 h-0.5 bg-black dark:bg-white mb-1"></span>
+                        <span className="block w-6 h-0.5 bg-black dark:bg-white"></span>
+                    </button>
+                </div>
+                <ul className="flex-row md:flex md:static md:flex-row md:items-center hidden">
+                    <li className="mx-2 my-auto md:mx-8">Projects</li>
+                    <li className="mx-2 my-auto md:mx-8">Contact Me</li>
+                    <li className="mx-2 my-auto md:mx-8">Download CV</li>
+                </ul>
+                {menuOpen && (
+                    <ul className="flex flex-col bg-white dark:bg-black text-black dark:text-white absolute right-0 top-20 w-40 shadow-lg md:hidden z-50">
+                        <li className="px-4 py-2">Projects</li>
+                        <li className="px-4 py-2">Contact Me</li>
+                        <li className="px-4 py-2">Download CV</li>
+                    </ul>
+                )}
+            </div>
+            <div className="body" class="text-gray-500 mt-500 w-4/5 md:my-auto ">
+                <p >Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
